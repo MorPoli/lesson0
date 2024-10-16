2d29c607e45405f83953cc3fd9bc7b5b49d5da05
Nik <130367969+MorPoli@users.noreply.github.com>
2024-10-16 18:56:52 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,17 @@
+my_list = ['guitar', 'bass', 'drums', 'piano', 'kik', 'tom' ]
+
+print('List: ', my_list)
+print('First element: ', my_list[0])
+print('Last element: ', my_list[-1])
+print('Slice: ', my_list[2:5])
+my_list[2] = "drum' n' bass"
+print('Modified list: ', my_list)
+
+
+my_dict = {'A': 440, 'B': 494, 'C': 523}
+
+print('Dict: ', my_dict)
+print('Value for key "A" is: ',my_dict['A'])
+my_dict['D'] = 587
+print('Modified dict: ', my_dict)
+
